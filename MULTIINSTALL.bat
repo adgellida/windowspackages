a615b4254fd534f934a10280965807ff99705b56
Antonio David Gellida Lavara <adgellida@gmail.com>
2015-09-21 20:42:19 +0200

    "Updating"

--- MULTIINSTALL.bat
+++ MULTIINSTALL.bat
@@ -4,17 +4,17 @@ rem multi-installer, select programs to install
 rem cmd version - multiselection available
 
 rem Backup & Sync
-rem cinst dirsyncpro #to add
+rem cinst -y dirsyncpro #to add
 
 rem OS & Utilities
-cinst winrar notepadplusplus.install classic-shell teamviewer
-rem clover ccleaner silverlight peazip 
+cinst -y notepadplusplus.install classic-shell teamviewer #winrar - to fix
+rem clover ccleaner silverlight peazip #not necessary
 
 rem Security & Privacy
-rem cinst 360ts #to update & to add 360tse
+rem cinst -y 360ts #to update & to add 360tse
 
 rem CD/DVD Tools
-rem cinst imgburn
+rem cinst -y imgburn
 
 rem Photos & Graphics
 
@@ -23,7 +23,7 @@ rem Audio & Music
 
 
 rem Videos & Movies
-cinst vlc
+cinst -y vlc
 
 rem Audio & Music & Videos & Movies
 
@@ -32,30 +32,30 @@ rem Development
 
 
 rem Web Browsers
-cinst google-chrome-x64 adblockplusie
+cinst -y google-chrome-x64 #adblockplusie - not necessary on edge
 rem googlechrome.dev
-rem cinst googlechrome
-rem cinst ublock #to add
-rem cinst adblockpluschrome
+rem googlechrome
+rem cinst -y ublock #to add
+rem cinst -y adblockpluschrome #changed by ublock
 
 rem Social
-rem cinst skype
+rem cinst -y skype
 
 rem File Management
 
 
 rem File Sharing
-cinst transmission-qt
+cinst -y transmission-qt
 
 rem News & Books
 
 
 rem Office & Productivity
-cinst sumatrapdf.install libreoffice
+cinst -y sumatrapdf.install libreoffice
 
 rem Games
 
 
 rem Base command
 
-rem cinst pkg1 pkg2 pkgN
+rem cinst -y pkg1 pkg2 pkgN
