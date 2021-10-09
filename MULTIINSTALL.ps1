f72c64d6062d984023e0b0b82140e5ffbf0e0c8f
Antonio David Gellida Lavara <adgellida@gmail.com>
2021-10-10 00:50:24 +0200

    Fix order and repeated packages

--- MULTIINSTALL.ps1
+++ MULTIINSTALL.ps1
@@ -2,107 +2,106 @@
 #https://github.com/adgellida/windowspackages
 #GNU General Public License v2.0
 
+cinst -y aida64-extreme
+cinst -y androidstudio
+cinst -y anydesk.install
+cinst -y audacity
+cinst -y authy.install
+cinst -y blender
+cinst -y bluescreenview.install
+cinst -y brave
 cinst -y chocolateygui
-cinst -y megasync
-cinst -y open-shell
-cinst -y teamviewer
-cinst -y chocolateygui
-cinst -y winrar
-#cinst -y volume2.install
-cinst -y winsys
-cinst -y keepassxc
-cinst -y vlc
-cinst -y sublimetext4
-cinst -y smartgit
+cinst -y converseen
+cinst -y crystaldiskinfo.install
+cinst -y crystaldiskmark
+cinst -y deepl
+cinst -y discord.install
+cinst -y drawio
+cinst -y dropbox
+cinst -y element-desktop
+cinst -y epicgameslauncher
+cinst -y etcher
+cinst -y everything
+cinst -y filezilla
+cinst -y firefox
+cinst -y freecad
+cinst -y freefilesync
+cinst -y frostwire
 cinst -y google-chrome
+cinst -y jdownloader
+cinst -y jre8
+cinst -y keepassxc
+cinst -y kicad
+cinst -y krita
+cinst -y macrocreator.install
+cinst -y malwarebytes
+cinst -y megasync
+cinst -y msiafterburner
+cinst -y mtputty
+cinst -y openhardwaremonitor
+cinst -y open-shell
+cinst -y origin
+cinst -y parsec
+cinst -y partitionwizard
+cinst -y processhacker.install
+cinst -y putty.install
 cinst -y qbittorrent
 cinst -y skype
-cinst -y steam-client
-cinst -y telegram.install
-cinst -y freefilesync
-cinst -y audacity
-cinst -y drawio
-cinst -y crystaldiskmark
-cinst -y crystaldiskinfo.install
-cinst -y malwarebytes
-cinst -y vscode.install
-cinst -y stopupdates10
-cinst -y origin
-cinst -y everything
-cinst -y aida64-extreme
-cinst -y spotify
 cinst -y slack
-cinst -y parsec
-cinst -y filezilla
-cinst -y element-desktop
-cinst -y putty.install
-cinst -y blender
-cinst -y firefox
-cinst -y vivaldi
-cinst -y teamspeak
-cinst -y vscode
-cinst -y openhardwaremonitor
-cinst -y anydesk.install
-cinst -y discord.install
-cinst -y sumo
-cinst -y deepl
-cinst -y xampp-80
-cinst -y msiafterburner
-cinst -y unity-hub
-cinst -y partitionwizard
-cinst -y zoom
-cinst -y windirstat
-cinst -y voicemeeter-potato.install
-cinst -y winpcap
-cinst -y vysor
+cinst -y smartgit
+cinst -y spotify
+cinst -y steam-client
 cinst -y streamlabs-obs
+cinst -y sublimetext4
+cinst -y sumo
 cinst -y supertuxkart
+cinst -y teamspeak
+cinst -y teamviewer
+cinst -y telegram.install
+cinst -y unity-hub
 cinst -y virtualbox virtualbox-guest-additions-guest.install
-cinst -y bluescreenview.install
-cinst -y mtputty
-cinst -y kicad
-cinst -y jdownloader
-cinst -y freecad
-cinst -y frostwire
-cinst -y jre8
-cinst -y dropbox
-cinst -y epicgameslauncher
-cinst -y converseen
-cinst -y brave
-cinst -y authy.install
-cinst -y androidstudio
-cinst -y etcher
-cinst -y macrocreator.install
-cinst -y processhacker.install
-cinst -y krita
+cinst -y vivaldi
+cinst -y vlc
+cinst -y voicemeeter-potato.install
+cinst -y volume2.install
+cinst -y vscode.install
+cinst -y vysor
+cinst -y windirstat
+cinst -y winpcap
+cinst -y winrar
+cinst -y winsys
+cinst -y xampp-80
+cinst -y zoom
+
 
 ##not exist
+#converseen
 #kasperskysecurityclod
 #prismlivestudio
-#yawcam
 #revolt
-#converseen
+#stopupdates10
+#yawcam
 
 ##others
-#spybot
-#superantispyware
-#obs-studio.install
-#stopupdates10
 #acestream
-#sumatrapdf.install
-#libreoffice
-#adblockplusie #not necessary on edge
-#googlechrome.dev
-#googlechrome
-#ublock #to add
 #adblockpluschrome #changed by ublock
-#hyper
-#notepadplusplus.install
-#nettime
-#clover
+#adblockplusie #not necessary on edge
 #ccleaner
-#silverlight
+#choco-upgrade-all-at-startup
+#clover
+#dirsyncpro
+#googlechrome
+#googlechrome.dev
+#hyper
+#libreoffice
+#nettime
+#notepadplusplus.install
+#obs-studio.install
 #peazip
 #qtcreator
-#choco-upgrade-all-at-startup
-#dirsyncpro
+#silverlight
+#spybot
+#stopupdates10
+#sumatrapdf.install
+#superantispyware
+#ublock #to add
