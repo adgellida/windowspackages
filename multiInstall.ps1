07b35b62c6efdb90c8a799c143532bdf7c0564ba
adgellida <adgellida@gmail.com>
2024-03-04 00:18:05 +0100

    Updating

--- multiInstall.ps1
+++ multiInstall.ps1
@@ -2,105 +2,31 @@
 #https://github.com/adgellida/windowspackages
 #GNU General Public License v2.0
 
-choco install -y aida64-extreme
-#choco install -y androidstudio
-#choco install -y anydesk.install
-#choco install -y audacity
-choco install -y authy.install
-#choco install -y blender
-choco install -y bluescreenview.install
-#choco install -y brave
-choco install -y chocolateygui
-#choco install -y converseen
-choco install -y crystaldiskinfo.install
-#choco install -y crystaldiskmark
-#choco install -y deepl
-choco install -y discord.install
-#choco install -y drawio
-choco install -y dropbox
-#choco install -y element-desktop
-#choco install -y epicgameslauncher
-#choco install -y etcher
-#choco install -y everything
-#choco install -y filezilla
-choco install -y firefox
-#choco install -y freecad
-choco install -y freefilesync
-#choco install -y frostwire
-choco install -y googlechrome
-choco install -y jdownloader
-#choco install -y jre8
-choco install -y keepassxc
-#choco install -y kicad
-#choco install -y krita
-#choco install -y macrocreator.install
-choco install -y malwarebytes
-#choco install -y megasync
-#choco install -y msiafterburner
-#choco install -y mtputty
-#choco install -y notepadplusplus.install
-#choco install -y openhardwaremonitor
-#choco install -y open-shell
-choco install -y origin
-#choco install -y parsec
-choco install -y partitionwizard
-#choco install -y processhacker.install
-#choco install -y putty.install
-choco install -y qbittorrent
-choco install -y rustdesk
-#choco install -y skype
-#choco install -y slack
-#choco install -y smartgit
-choco install -y spotify
-choco install -y steam-client
-#choco install -y streamlabs-obs
-#choco install -y sublimetext4
-choco install -y sumo
-#choco install -y supertuxkart
-#choco install -y teamspeak
-#choco install -y teamviewer
-choco install -y telegram.install
-#choco install -y unity-hub
-#choco install -y virtualbox virtualbox-guest-additions-guest.install
-#choco install -y vivaldi
-choco install -y vlc
-#choco install -y voicemeeter-potato.install
-choco install -y volume2.install
-choco install -y vscode.install
-#choco install -y vysor
-choco install -y windirstat
-#choco install -y winpcap
-choco install -y winrar
-#choco install -y winsys
-#choco install -y xampp-80
-#choco install -y zoom
+$choco_install = choco install -y --ignore-checksums
 
-
-##not exist
-#converseen
-#kasperskysecuritycloud
-#prismlivestudio
-#revolt
-#stopupdates10
-#yawcam
-
-##others
-#acestream
-#adblockpluschrome #changed by ublock
-#adblockplusie #not necessary on edge
-#ccleaner
-#choco-upgrade-all-at-startup
-#clover
-#dirsyncpro
-#hyper
-#libreoffice
-#nettime
-#obs-studio.install
-#peazip
-#qtcreator
-#silverlight
-#spybot
-#stopupdates10
-#sumatrapdf.install
-#superantispyware
-#ublock #to add
+$choco_install adwcleaner
+$choco_install aida64-extreme
+#$choco_install authy.install
+$choco_install bluescreenview.install
+$choco_install chocolateygui
+$choco_install crystaldiskinfo.install
+#$choco_install discord.install
+#$choco_install dropbox
+$choco_install firefox
+#$choco_install freefilesync
+$choco_install googlechrome
+$choco_install jdownloader
+$choco_install keepassxc
+$choco_install malwarebytes
+$choco_install notepadplusplus.install
+$choco_install partitionwizard
+$choco_install qbittorrent
+$choco_install rustdesk
+$choco_install spotify
+#$choco_install steam-client
+$choco_install telegram.install
+$choco_install vlc
+$choco_install volume2.install
+#$choco_install vscode.install
+$choco_install windirstat
+$choco_install winrar
