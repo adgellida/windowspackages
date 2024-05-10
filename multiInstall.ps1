fd244da695749487a350dca218f8fa2c7c482a3f
adgellida <adgellida@gmail.com>
2024-05-11 01:45:37 +0200

    Fix

--- multiInstall.ps1
+++ multiInstall.ps1
@@ -2,31 +2,33 @@
 #https://github.com/adgellida/windowspackages
 #GNU General Public License v2.0
 
-$choco_install = choco install -y --ignore-checksums
+$packageNames = @(
 
-$choco_install adwcleaner
-$choco_install aida64-extreme
-#$choco_install authy.install
-$choco_install bluescreenview.install
-$choco_install chocolateygui
-$choco_install crystaldiskinfo.install
-#$choco_install discord.install
-#$choco_install dropbox
-$choco_install firefox
-#$choco_install freefilesync
-$choco_install googlechrome
-$choco_install jdownloader
-$choco_install keepassxc
-$choco_install malwarebytes
-$choco_install notepadplusplus.install
-$choco_install partitionwizard
-$choco_install qbittorrent
-$choco_install rustdesk
-$choco_install spotify
-#$choco_install steam-client
-$choco_install telegram.install
-$choco_install vlc
-$choco_install volume2.install
-#$choco_install vscode.install
-$choco_install windirstat
-$choco_install winrar
+    "adwcleaner"
+    "aida64-extreme"
+    "bluescreenview.install"
+    "chocolateygui"
+    "crystaldiskinfo.install"
+    "discord.install"
+    "dropbox"
+    "files"
+    "firefox"
+    "googlechrome"
+    "jdownloader"
+    "keepassxc"
+    "malwarebytes"
+    "partitionwizard"
+    "qbittorrent"
+    "spotify"
+    "telegram.install"
+    "vlc"
+    "volume2.install"
+    "vscode.install"
+    "windirstat"
+    "winrar"
+)
+
+foreach ($packageName in $packageNames) {
+    $choco_install = "choco install -y --ignore-checksums $packageName"
+    Invoke-Expression $choco_install
+}
